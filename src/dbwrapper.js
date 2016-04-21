bb45c0af4e3ccba8fbd9ff30040914c59a93f99b
Ahmad Ben Mrad <batikhsouri@gmail.com>
2016-04-21 12:23:00 +0200

    Adding a mandatory clearAll function to DBWrapper, clear the databases between workloads

--- src/dbwrapper.js
+++ src/dbwrapper.js
@@ -1,4 +1,4 @@
-function DBWrapper(dbNameS, findFn, findOneFn, saveFn, bulkSaveFn, updateFn, removeFn){
+function DBWrapper(dbNameS, findFn, findOneFn, saveFn, bulkSaveFn, updateFn, removeFn, clearAllFn){
 	if (typeof dbNameS != 'string') throw new TypeError('dbNameS must be a string');
 	if (typeof findFn != 'function') throw new TypeError('findFn must be a function');
 	if (typeof findOneFn != 'function') throw new TypeError('findOneFn must be a function');
@@ -6,6 +6,7 @@ function DBWrapper(dbNameS, findFn, findOneFn, saveFn, bulkSaveFn, updateFn, rem
 	if (typeof bulkSaveFn != 'function') throw new TypeError('bulkSaveFn must be a function');
 	if (typeof updateFn != 'function') throw new TypeError('updateFn must be a function');
 	if (typeof removeFn != 'function') throw new TypeError('removeFn must be a function');
+	if (typeof clearAllFn != 'function') throw new TypeError('clearAllFn must be a function');
 
 	// One Db wrapper will be instanciated for every DB that we benchmark
 	this.dbName = dbNameS;
@@ -15,4 +16,5 @@ function DBWrapper(dbNameS, findFn, findOneFn, saveFn, bulkSaveFn, updateFn, rem
 	this.bulkSave = bulkSaveFn;
 	this.update = updateFn;
 	this.remove = removeFn;
+	this.clearAll = clearAllFn;
 }
