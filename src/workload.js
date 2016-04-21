40642ea2ec20633632d86dd00d00a55afc2b27a1
Ahmad Ben Mrad <batikhsouri@gmail.com>
2016-04-21 16:45:37 +0200

    Reorganizing the workload "class"

--- src/workload.js
+++ src/workload.js
@@ -1,20 +1,21 @@
-function Workload(generateFunction, runnerFunction, name){
+function Workload(dbWrappers, generateFunction, runnerFunction, name){
+	if (!(Array.isArray(dbWrappers) && dbWrappers.length > 0)) throw new TypeError('dbWrappers must be an array');
+	dbWrappers.forEach(function(item, index){
+		if (!(item instanceof DBWrapper)) throw new TypeError('dbWrappers[' + index + '] must be an instance of DBWrapper');
+	});
+
 	if (typeof generateFunction != 'function') throw new TypeError('generateFunction must be a function');
 	if (typeof runnerFunction != 'function') throw new TypeError('runnerFunction must be a function');
 	if (typeof name != 'string') throw new TypeError('name must be a string');
 
 	/**
-	* generateFunction([Function cb])
-	* runnerFunction(Object dbMethodsWrappers | DBWrapper dbwInstance)
-	* Where dbMethodsWrappers contain
-	* - find
-	* - findOne
-	* - save
-	* - bulkSave
-	* - update
-	* - remove
+	* DBWrapper instances
+	* generateFunction(Function cb)
+	* runnerFunction(Function cb)
+	* String workloadName
 	*/
 
+	this._dbDrivers = dbWrappers;
 	this._generateFunction = generateFunction;
 	this._runnerFunction = runnerFunction;
 	this._name = name;
