2653c14de9de3b9c0c95ae2314b08baf8e772958
Ahmad Ben Mrad <batikhsouri@gmail.com>
2016-04-25 01:18:41 +0200

    Some data generation functions

--- src/workloadGenerator.js
+++ src/workloadGenerator.js
@@ -1,4 +1,8 @@
 function WorkloadGenerator(dbWrappers, _workloadOptions){
+	var from_string = sodium.from_string, to_string = sodium.to_string;
+	var from_base64 = sodium.from_base64, to_base64 = sodium.to_base64;
+	var from_hex = sodium.from_hex, to_hex = sodium.to_hex;
+
 	checkDBWrapperArray(dbWrappers);
 
 	if (_workloadOptions && typeof _workloadOptions != 'object') throw new TypeError('when defined, _workloadOptions must be an object');
@@ -6,7 +10,8 @@ function WorkloadGenerator(dbWrappers, _workloadOptions){
 	var workloadOptionsDefaults = {
 		fieldCount: 10,
 		fieldSize: 100,
-		docCount: 5000
+		docCount: 5000,
+		generateId: true
 	};
 
 	var workloadOptions = {};
@@ -17,6 +22,59 @@ function WorkloadGenerator(dbWrappers, _workloadOptions){
 	//Using workloadOptionsDefaults for undefined mandatory parameters
 	for (var propName in workloadOptionsDefaults){
 		workloadOptions[propName] = workloadOptions[propName] || workloadOptionsDefaults[propName];
+	}
+
+	function generateDoc(){
+		var genFieldsCount = 0;
+
+		var d = {};
+
+		if (workloadOptions.generateId){
+			d._id = generateString(workloadOptions.fieldSize);
+			genFieldsCount++;
+		}
+
+		while (genFieldsCount < workloadOptions.fieldCount){
+			d[generateString(4)] = generateString(workloadOptions.fieldSize);
+			genFieldsCount++;
+		}
+
+		return d;
+	}
+
+	function generateBlob(){
+		var bKbSize = 100 + Math.round(Math.random() * 100);
+		return generateBuffer(bKbSize * 1024);
+	}
+
+	function generateString(length, type){
+		type = type || 'hex';
+
+		if (!(typeof length == 'number' && Math.floor(length) == length && length > 0)) throw new TypeError('length must be strictly positive integer number');
+
+		if (!(type == 'hex' || type == 'base64' || type == 'alphanum' || 'raw')){
+			throw new TypeError('invalid string type: ' + type);
+		}
+
+		if (type == 'hex'){
+			return to_hex(generateBuffer(Math.ceil(length / 2))).substr(0, length);
+		} else if (type == 'base64'){
+			return to_base64(generateBuffer(Math.ceil((3 / 4) * length))).substr(0, length);
+		} else if (type == 'alphanum'){
+			//Worst case for entropy, but who cares in this case...
+			var charset = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789';
+			var s = '';
+			for (var i = 0; i < length; i++) s += charset[Math.floor(Math.random() * charset.length)];
+			return s;
+		} else {
+			return to_string(generateBuffer(length));
+		}
+	}
+
+	function generateBuffer(length){
+		var b = new Uint8Array(length);
+		window.crypto.getRandomValues(b);
+		return b;
 	}
 
 	function generateFunctionFactory(){
