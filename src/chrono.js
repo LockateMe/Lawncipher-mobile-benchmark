e27af7387ddc864f4ee4b1320c770c3d3940abf2
Ahmad Ben Mrad <batikhsouri@gmail.com>
2016-04-24 20:38:27 +0200

    Adding a Chronometer object, to allow finer-grained time measurements

---
+++ src/chrono.js
@@ -0,0 +1,51 @@
+/**
+* A chronometer object, hoping to provide fine-grained time measurements
+*/
+
+function Chrono(){
+	var totalDuration = 0;
+	var interval;
+	var startTime, stopTime;
+
+	/**
+	* Start a time measurement
+	* If a time measurement is already ongoing, this method doesn't do anything (not even throwing an error)
+	*/
+	this.start = function(){
+		startTime = startTime || Date.now();
+	};
+
+	/**
+	* Stop an ongoing time measurement and adds this latest measurement's elapsed time to elapsedTime
+	* Returns -1 if no time measurement is ongoing
+	* Returns the total elasped time otherwise
+	*/
+	this.stop = function(){
+		if (!startTime) return -1;
+		stopTime = Date.now();
+
+		interval = stopTime - startTime;
+		totalDuration += interval;
+		startTime = 0;
+
+		return totalDuration;
+	};
+
+	/**
+	* Returns the total elasped time (summing all "measurements" done this instance)
+	* Returns -1 if a measurement is ongoing
+	*/
+	this.elapsedTime = function(){
+		if (startTime) return -1;
+		return totalDuration;
+	};
+
+	/**
+	* Reset the Chrono instance
+	* Doesn't reset the instance if a measurement is ongoing (and returns -1 in this case)
+	*/
+	this.reset = function(){
+		if (startTime) return -1;
+		totalDuration = 0;
+	};
+}
